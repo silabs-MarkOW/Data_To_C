4de251c352396a79e22552b713d2b1f472898da3
silabs-MarkOW <41750418+silabs-MarkOW@users.noreply.github.com>
2023-06-01 13:38:17 -0500

    Add actual code to project

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
 # Data_To_C
 Generate code to include binary image or arbitrary size arrays in .rodata or .data.
+
+# Example Usage
+<pre>
+$ python3 data-to-c.py
+Error: expecting single parameter, <base-filename>
+Usage: data-to-c.py [ options ] <base-filename>
+  options:
+          --rodata=<size,name>     generate <size> bytes sized variable called
+                                   <name> in .rodata
+            --data=<size,name>     generate <size> bytes sized variable called
+                                   <name> in .data
+    --binary-image=<filename,name> place contents of <filename> in variable
+                                   called <name> in .rodata
+</pre>
+
+## Include binary file as array
+Useful for testing bootloader API.
+<pre>
+$ python3 data-to-c.py --binary-image=lzma.gbl,gbl_image data
+</pre>
+<pre>
+$ cat data.h
+#ifndef DATA_H
+#define DATA_H
+
+#include <stdint.h>
+
+extern const uint32_t gbl_image[28648];
+
+#endif
+</pre>
+<pre>
+$ head data.c
+#include "data.h"
+
+const uint32_t gbl_image[28648] = {
+  0x03a617eb,
+  0x00000008,
+  0x03000000,
+  0x00000000,
+  0xf40a0af4,
+  0x0000001c,
+  0x00000020,
+</pre>
+
+## Create 8 bytes in .data and 16 bytes in .rodata
+<pre>
+$ python3 data-to-c.py --data=8,eight_bytes --rodata=16,sixteen_bytes data
+</pre>
+<pre>
+$ cat data.h
+#ifndef DATA_H
+#define DATA_H
+
+#include <stdint.h>
+
+extern const uint32_t sixteen_bytes[4];
+extern uint32_t eight_bytes[2];
+
+#endif
+</pre>
---
+++ data-to-c.py
@@ -0,0 +1,172 @@
+import sys
+import getopt
+import shutil
+import numpy
+
+long_opts = []
+options = {}
+option_types = {}
+option_params = {}
+option_desc = {}
+
+def add_option(name, datatype=None, default=None, params=None, desc=None) :
+    option = name
+    if None != datatype :
+        option += '='
+        if None == params :
+            raise RuntimeError('option "%s" missing params'%(name))
+        else :
+            option_params[name] = params
+    long_opts.append(option)
+    if None != default :
+        if type(default) != datatype :
+            raise RuntimeError('type(%s) != %s'%(default.__str__(),str(datatype)))
+        options[name] = default
+    option_types[name] = datatype
+    if None != desc :
+        option_desc[name] = desc
+    
+add_option('rodata',(int,str),params="<size,name>",desc="generate <size> bytes sized variable called <name> in .rodata")
+add_option('data',(int,str),params="<size,name>",desc="generate <size> bytes sized variable called <name> in .data")
+add_option('binary-image',(str,str),params="<filename,name>",desc="place contents of <filename> in variable called <name> in .rodata")
+
+def format(s,width) :
+    length = len(s)
+    if length <= width : return [s]
+    lines = []
+    tokens = s.split()
+    current = tokens.pop(0)
+    while len(tokens) :
+        if len(current) + 1 + len(tokens[0]) > width :
+            lines.append(current)
+            current = tokens.pop(0)
+        else :
+            current += ' ' + tokens.pop(0)
+    lines.append(current)
+    return lines
+
+def exit_help(msg = None) :
+    width = shutil.get_terminal_size((80, 20))[0]
+    if None != msg :
+        print('Error: %s'%(msg))
+    print('Usage: %s [ options ] <base-filename>\n  options:'%(sys.argv[0]))
+    optlen = 0
+    paramlen = 0
+    for option in long_opts :
+        if '=' == option[-1] :
+            name = option[:-1]
+        else :
+            name = option
+        if len(option_params[name]) > paramlen : paramlen = len(option_params[name])
+        if len(option) > optlen : optlen = len(option)
+    fmt = '    %%%ds%%-%ds %%s'%(optlen+2,paramlen)
+    for option in long_opts :
+        if '=' == option[-1] :
+            name = option[:-1]
+        else :
+            name = option
+        desc = option_desc.get(name)
+        if None == desc : desc = ''
+        desc = format(desc,width-len(fmt%('','','')))
+        print(fmt%('--'+option,option_params[name],desc[0]))
+        for d in desc[1:] :
+            print(fmt%('','',d))
+    quit()
+    
+opts,params = getopt.getopt(sys.argv[1:],'h',long_opts)
+for opt,param in opts :
+    if '-h' == opt :
+        exit_help()
+    else :
+        for option in long_opts :
+            if '=' == option[-1] :
+                name = option[:-1]
+                if '--'+name == opt :
+                    datatype = option_types[name]
+                    if tuple == type(datatype) :
+                        tokens = param.split(',')
+                        if len(tokens) != len(datatype) :
+                            exit_help('tuple length mismatch parsing "%s" parameters.  Expecting %s, got %s'%(opt,datatype.__str__(),tokens.__str__()))
+                        option = ()
+                        for i in range(len(tokens)) :
+                            option += (datatype[i](tokens[i]),)
+                        options[name] = option
+                    else :
+                        options[name] = datatype(param)
+
+if len(params) != 1 :
+    exit_help('expecting single parameter, <base-filename>')
+
+class Data_Source :
+    def __init__(s,param) :
+        if int == type(param) :
+            s.data = numpy.random.bytes(size)
+        elif str == type(param) :
+            fh = open(filename,'rb')
+            s.data = fh.read()
+            fh.close()
+        else :
+            raise RuntimeError(type(param))
+        rem = len(s.data) & 3
+        if 0 == rem :
+            s.type = "uint32_t"
+            s.step = 4
+        elif 2 & rem :
+            s.type = "uint16_t"
+            s.step = 2
+        else :
+            s.type = "uint8_t"
+            s.step = 1
+    def set_type(s,ctype) :
+        raise RuntimeError('Not impemented')
+    def get_type(s) :
+        return s.type
+    def get_items(s) :
+        item_count = len(s.data) // s.step
+        items = []
+        format = '0x%%0%dx'%(s.step<<1)
+        for i in range(item_count) :
+            bin = s.data[s.step*i:s.step*(i+1)]
+            num = int.from_bytes(bin,'little')
+            items.append(format%(num))
+        return items
+            
+basename = params[0]
+c_content = ''
+h_content = ''
+
+option = options.get('binary-image')
+if None != option :
+    filename,varname = option
+    ds = Data_Source(filename)
+    items = ds.get_items()
+    decl = 'const %s %s[%d]'%(ds.get_type(),varname,len(items))
+    h_content += 'extern %s;\n'%(decl)
+    c_content += '%s = {\n  %s\n};\n\n'%(decl,',\n  '.join(items))
+    
+option = options.get('rodata')
+if None != option :
+    size,varname = option
+    ds = Data_Source(size)
+    items = ds.get_items()
+    decl = 'const %s %s[%d]'%(ds.get_type(),varname,len(items))
+    h_content += 'extern %s;\n'%(decl)
+    c_content += '%s = {\n  %s\n};\n\n'%(decl,',\n  '.join(items))
+    
+option = options.get('data')
+if None != option :
+    size,varname = option
+    ds = Data_Source(size)
+    items = ds.get_items()
+    decl = '%s %s[%d]'%(ds.get_type(),varname,len(items))
+    h_content += 'extern %s;\n'%(decl)
+    c_content += '%s = {\n  %s\n};\n\n'%(decl,',\n  '.join(items))
+
+define = basename.upper()+'_H'
+fh = open(basename+'.h','w')
+fh.write('#ifndef %s\n#define %s\n\n#include <stdint.h>\n\n%s\n#endif\n'%(define,define,h_content))
+fh.close()
+
+fh = open(basename+'.c','w')
+fh.write('#include "%s.h"\n\n%s'%(basename,c_content))
+fh.close()
